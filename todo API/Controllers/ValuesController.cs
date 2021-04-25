ca936bcd6c325bcf133b05181b8baa34be5f4a34
James.Sampedro <72242960+Votum38git@users.noreply.github.com>
2021-04-25 20:00:27 +0800

    ok

---
+++ todo API/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace test_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+     
+        private List<Todo> todos = new List<Todo>();
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            todos.Add(new Todo { id = Guid.NewGuid(), text = "Eat" });
+            todos.Add(new Todo { id = Guid.NewGuid(), text = "Sleep" });
+            todos.Add(new Todo { id = Guid.NewGuid(), text = "Walk" });
+            return Ok(todos);
+        }
+
+       
+    }
+
+    public class Todo
+    {
+        public Guid id { get; set; }
+        public string text { get; set; }
+        public bool completed { get; set; } = false;
+    }
+
+}
